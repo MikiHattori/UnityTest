06762925adfb739916c124ab58de60ba059b7662
MikiHattori <dgs.hibiki7@gmail.com>
2018-02-01 01:32:45 +0900

    Add CameraController.cs

---
+++ UnityTest/Assets/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject cube;
+
+    // Use this for initialization
+    void Start()
+    {
+        //GetComponent<Rigidbody>().velocity = transform.right;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(cube.transform);
+        if (Vector3.Distance(transform.position, cube.transform.position) > 10)
+        {
+            transform.position += transform.forward * 0.5f;
+        }
+        else
+        {
+            transform.position += transform.right * 0.1f;
+        }
+
+
+    }
+}
