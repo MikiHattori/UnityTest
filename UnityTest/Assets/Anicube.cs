8545494d11ea36869d0d60fe8bc06dfcaa2ec572
MikiHattori <dgs.hibiki7@gmail.com>
2018-02-01 19:07:55 +0900

    Add Animation

---
+++ UnityTest/Assets/Anicube.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Anicube : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Return))
+        {
+            GetComponent<Animator>().SetBool("isJump", true);
+            GetComponent<Animator>().applyRootMotion = false;
+        }
+	}
+
+    void Land()
+    {
+        GetComponent<Animator>().SetBool("isJump", false);
+        GetComponent<Animator>().applyRootMotion = true;
+
+    }
+}
